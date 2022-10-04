77e29f4e8496dff8bd83ca9fc36f38197d3703ea
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2022-10-04 17:41:22 -0300

    Use getJSONObject to get inner field

--- src/main/kotlin/me/brunosantana/JSONObjectExamples.kt
+++ src/main/kotlin/me/brunosantana/JSONObjectExamples.kt
@@ -5,9 +5,22 @@ import org.json.JSONObject
 fun main() {
     //https://www.baeldung.com/java-org-json
     //https://github.com/eugenp/tutorials/blob/master/json-modules/json/src/test/java/com/baeldung/jsonjava/JSONObjectIntegrationTest.java
+    //https://stackoverflow.com/questions/14898768/how-to-access-nested-elements-of-json-object-using-getjsonarray-method
 
     val jo = JSONObject(
-        "{\"city\":\"chicago\",\"name\":\"jon doe\",\"age\":\"22\"}"
+        "{" +
+                    "\"city\":\"chicago\"," +
+                    "\"name\":\"jon doe\"," +
+                    "\"age\":\"22\"," +
+                    "\"data\": {" +
+                        "\"response\": {" +
+                            "\"id\":10," +
+                            "\"content\": {" +
+                                "\"message\": \"my message\"" +
+                            "}" +
+                        "}" +
+                    "}" +
+                "}"
     );
 
     println(jo)
@@ -15,4 +28,13 @@ fun main() {
     println(jo.get("city"))
     println(jo.get("name"))
     println(jo.get("age"))
+    println(jo.get("data"))
+    println(jo.getJSONObject("data").get("response"))
+    println(jo.getJSONObject("data").getJSONObject("response").get("id"))
+    println(jo.getJSONObject("data").getJSONObject("response").getInt("id"))
+    println(jo.getJSONObject("data").getJSONObject("response").get("content"))
+    println(jo.getJSONObject("data").getJSONObject("response").getJSONObject("content").get("message"))
+
+    val response: JSONObject = jo.getJSONObject("data").get("response") as JSONObject
+    println(response.toString())
 }
