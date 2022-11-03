4d9815db15915890c595dbcc54e9ad8817e4abef
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2022-11-03 10:59:07 -0300

    Varargs examples

---
+++ src/main/kotlin/me/brunosantana/VarargsExamples.kt
@@ -0,0 +1,21 @@
+package me.brunosantana
+
+//https://stackoverflow.com/questions/44239869/whats-the-kotlin-equivalent-of-javas-string
+//https://www.techiedelight.com/add-new-element-array-kotlin/
+
+fun main() {
+    test("item1", "item2", "item3")
+
+    val a = arrayOf("item4", "item5", "item6")
+    test(*a)
+
+    var b = arrayOf<String>()
+    b = b.plus("item7")
+    b = b.plus("item8")
+    b = b.plus("item9")
+    test(*b)
+}
+
+fun test(vararg names: String){
+    names.forEach { println(it) }
+}
