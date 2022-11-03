e1fd0312253744ad0f656b462f1656ba4e0adbc7
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2022-11-02 21:15:34 -0300

    Examples of JSONArray

--- src/main/kotlin/me/brunosantana/JSONObjectExamples.kt
+++ src/main/kotlin/me/brunosantana/JSONObjectExamples.kt
@@ -1,11 +1,21 @@
 package me.brunosantana
 
+import org.json.JSONArray
 import org.json.JSONObject
 
 fun main() {
     //https://www.baeldung.com/java-org-json
     //https://github.com/eugenp/tutorials/blob/master/json-modules/json/src/test/java/com/baeldung/jsonjava/JSONObjectIntegrationTest.java
     //https://stackoverflow.com/questions/14898768/how-to-access-nested-elements-of-json-object-using-getjsonarray-method
+
+    /*
+    JSONArray
+    https://www.baeldung.com/java-org-json#jsonarray
+    https://github.com/eugenp/tutorials/blob/master/json-modules/json/src/main/java/com/baeldung/jsonjava/JSONArrayDemo.java
+    https://github.com/eugenp/tutorials/blob/master/json-modules/json/src/main/java/com/baeldung/jsonjava/JSONArrayGetValueByKey.java
+    https://github.com/eugenp/tutorials/blob/master/json-modules/json/src/test/java/com/baeldung/jsonjava/JSONArrayGetValueByKeyUnitTest.java
+    https://github.com/eugenp/tutorials/blob/master/json-modules/json/src/test/java/com/baeldung/jsonjava/JSONArrayIntegrationTest.java
+     */
 
     val jo = JSONObject(
         "{" +
@@ -19,6 +29,31 @@ fun main() {
                                 "\"message\": \"my message\"" +
                             "}" +
                         "}" +
+                    "}," +
+                    "\"USA\": {" +
+                        "\"states\": [" +
+                            "\"Alabama\", \"Alaska\", \"Arizona\", \"Arkansas\"" +
+                        "]" +
+                    "}," +
+                    "\"Brazil\": {" +
+                        "\"states\": [" +
+                            "{" +
+                                "\"acronym\": \"SP\"," +
+                                "\"name\": \"São Paulo\"" +
+                            "}," +
+                            "{" +
+                                "\"acronym\": \"BA\"," +
+                                "\"name\": \"São Paulo\"" +
+                            "}," +
+                            "{" +
+                                "\"acronym\": \"MG\"," +
+                                "\"name\": \"Minas Gerais\"" +
+                            "}," +
+                            "{" +
+                                "\"acronym\": \"PE\"," +
+                                "\"name\": \"Pernambuco\"" +
+                            "}" +
+                        "]" +
                     "}" +
                 "}"
     );
@@ -34,6 +69,10 @@ fun main() {
     println(jo.getJSONObject("data").getJSONObject("response").getInt("id"))
     println(jo.getJSONObject("data").getJSONObject("response").get("content"))
     println(jo.getJSONObject("data").getJSONObject("response").getJSONObject("content").get("message"))
+    println(jo.getJSONObject("USA"))
+    println(jo.getJSONObject("USA").getJSONArray("states"))
+    println(jo.getJSONObject("Brazil"))
+    println(jo.getJSONObject("Brazil").getJSONArray("states"))
 
     val response: JSONObject = jo.getJSONObject("data").get("response") as JSONObject
     println(response.toString())
@@ -43,4 +82,19 @@ fun main() {
     val cityString: String = jo.getString("city")
     println(cityString)
     //val itDoesNotExist: String = jo.getString("itDoesNotExist") //throws org.json.JSONException
+
+    //iterate over JSONArray items
+    val usaJsonArray: JSONArray = jo.getJSONObject("USA").getJSONArray("states")
+    for (index in 0 until usaJsonArray.length()) {
+        val state: String = usaJsonArray.getString(index)
+        println(state)
+    }
+
+    val brazilJsonArray: JSONArray = jo.getJSONObject("Brazil").getJSONArray("states")
+    for (index in 0 until brazilJsonArray.length()) {
+        val state: JSONObject = brazilJsonArray.getJSONObject(index)
+        println(state)
+        println(state.getString("acronym"))
+        println(state.getString("name"))
+    }
 }
